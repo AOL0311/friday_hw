465bb342c0db760f44f18b19d18a95e77a7cd174
coca <cocashen5050@gmail.com>
2025-03-14 15:04:24 +0800

    ..

--- homework/2-1-1_1040062_王聖瑋.py
+++ homework/2-1-1_1040062_王聖瑋.py
@@ -1,5 +1,8 @@
 def calc(x):
-    return ((((x * 1000) - 1000) // 200) * 5) + 70
+    if x < 1:
+        return 70
+    else:
+        return ((((x * 1000) - 1000) // 200) * 5) + 70
 
 kilometer = float(input('輸入里程(km): '))
 
