b56cd389139d1a74250a8a10cb16330e96a842e0
Cocashen_MAC <cocashen5050@gmail.com>
2025-04-11 13:40:37 +0800

    ..

--- homework/2-2/2-2-2.py
+++ homework/2-2/2-2-2.py
@@ -5,4 +5,4 @@ regular = int(input('請輸入平時成績: '))
 mid_term = int(input('請輸入期中考成績: '))
 final = int(input('請輸入期末考成績: '))
 
-print(f'學期總成績為: {calc(regular, mid_term, final)}:.2f')
+print(f'學期總成績為: {calc(regular, mid_term, final):.2f}')
