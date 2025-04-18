4859f16341985220ba0f1ff104317f4d55f3cc73
AOL0311 <104348861+AOL0311@users.noreply.github.com>
2025-04-18 09:29:42 +0800

    Update 6-1_way_1.py

--- homework/6/6-1_way_1.py
+++ homework/6/6-1_way_1.py
@@ -1,9 +1,8 @@
-n = int(input('輸入要計算 n! 的 n 的值: '))
-
 def calc(x):
     if x <= 1:
         return 1
     else:
         return x * calc(x - 1)
-    
+        
+n = int(input('輸入要計算 n! 的 n 的值: '))
 print(calc(n))
