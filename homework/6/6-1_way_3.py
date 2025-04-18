14990cf11c925ddffb22293e7bec36efb8925d21
AOL0311 <104348861+AOL0311@users.noreply.github.com>
2025-04-18 09:31:40 +0800

    Update and rename 6-1w3.py to 6-1_way_3.py

---
+++ homework/6/6-1_way_3.py
@@ -0,0 +1,9 @@
+def calc(x):
+    result = 1
+    while x > 0:
+        result *= x
+        x -= 1
+    return result
+
+n = int(input('輸入要計算 n! 的 n 的值: '))
+print(calc(n)
--- homework/6/6-1w3.py
+++
@@ -1,9 +0,0 @@
-n = int(input('輸入要計算 n! 的 n 的值: '))
-
-ans = 1
-
-while n > 0:
-    ans *= n
-    n -= 1
-
-print(ans)
