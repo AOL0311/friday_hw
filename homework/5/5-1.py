8e7aaf07b2899b712bbd415715677a7b522e9f41
AOL0311 <cocashen5050@gmail.com>
2025-04-25 14:20:22 +0800

    Chapter 5

--- homework/5/5-1.py
+++ homework/5/5-1.py
@@ -2,16 +2,19 @@
 # 若不行，則輸出 無法形成三角形; 若可以，則判斷是下列何種三角形:
 # (1) 正三角形; (2) 等腰三角形; (3) 直角三角形; (4)普通三角形。
 
+def side_detect(x, y, z):
+    if x + y < z or x + z < y or y + z < x:
+        return '無法形成三角形'
+    else:
+        if x == b == c:
+            return '正三角形'
+        elif x == y or x == z or y == z:
+            return '等腰三角形'
+        elif x ** 2 + y ** 2 == z ** 2 or x ** 2 + z ** 2 == y ** 2 or y ** 2 + z ** 2 == x ** 2:
+            return '直角三角形'
+        else:
+            return '普通三角形'
+
 a, b, c = map(int, input('輸入一個由整數組成之序對 a, b, c: ').split(','))
 
-if a + b < c and a + c < b and b + c < a:
-    print('無法形成三角形')
-else:
-    if a == b == c:
-        print('正三角形')
-    elif a == b or a == c or b == c:
-        print('等腰三角形')
-    elif a ** 2 + b ** 2 == c ** 2 or a ** 2 + c ** 2 == b ** 2 or b ** 2 + c ** 2 == a ** 2:
-        print('直角三角形')
-    else:
-        print('普通三角形')
+print(side_detect(a, b, c))
--- homework/5/5-2.py
+++ homework/5/5-2.py
@@ -2,11 +2,14 @@
 # 請寫一程式，由鍵盤輸入氣溫 t，然後顯示現在溫度及狀態(狀態為: 開冷氣、開暖氣、不開機，3種)。
 # 例如: 輸入 30，顯示 "現在溫度為 30 度，開冷氣。"
 
+def temperature_detect(x):
+    if t < 15:
+        return f'現在溫度為 {t} 度，開暖氣。'
+    elif 28 < t:
+        return f'現在溫度為 {t} 度，開冷氣。'
+    else:
+        return f'現在溫度為 {t} 度，不開機。'
+
 t = int(input('輸入氣溫: '))
 
-if t < 15:
-    print(f'現在溫度為 {t} 度，開暖氣。')
-elif 28 < t:
-    print(f'現在溫度為 {t} 度，開冷氣。')
-else:
-    print(f'現在溫度為 {t} 度，不開機。')
+print(temperature_detect(t))
